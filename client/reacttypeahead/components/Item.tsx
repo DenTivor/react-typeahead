c727419cef21f747c57b86802905ddd1fad9044a
Roman Alekseev <roman.alekseev.ru@gmail.com>
2017-06-05 00:54:15 +0300

    add not-stable version

--- client/reacttypeahead/components/Item.tsx
+++ client/reacttypeahead/components/Item.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 
 interface ItemProps {
 	item?: any;
+	key?: any;
 }
 
 interface ItemState {
@@ -12,33 +13,39 @@ class Item extends React.Component<ItemProps, ItemState> {
 		super(props, context); 
 
 		this.state = {
-	      item: this.props.item || ''
+	      item: this.props.item
 	    }; 
+	}
+
+	handleClick() {
+		console.log("click");
 	}
 
 	render() {
 		const {name, nickname, tweets, following, followers, text} = this.props.item;
 
 	    return(
-			<div className="item clearfix">
-			  <div className="left-column pull-left">
-			    <div className="profile-image"></div>
-			  </div>
-			  <div className="right-column pull-left">
-			    <div className="top-block"><span className="name">{name}</span><span className="identification main-grey-color">{nickname}</span></div>
-			    <div className="center-block"><span className="info">{text}</span></div>
-			    <div className="bottom-block">
-			      <div className="social-activity">
-			        <div className="social-parameters"><span className="social-param-name main-grey-color">Tweets:</span><span className="social-counter">{tweets}</span></div>
-			        <div className="social-parameters"><span className="social-param-name main-grey-color">Following:</span><span className="social-counter">{following}</span></div>
-			        <div className="social-parameters"><span className="social-param-name main-grey-color">Followers:</span><span className="social-counter">{followers}</span></div>
-			      </div>
-			    </div>
-			  </div>
+	    	<div className="item-wrapper" onClick={this.handleClick.bind(this)}>
+				<div className="item clearfix" onClick={this.handleClick.bind(this)}>
+				  <div className="left-column pull-left">
+				    <div className="profile-image" onClick={this.handleClick.bind(this)}></div>
+				  </div>
+				  <div className="right-column pull-left" onClick={this.handleClick.bind(this)}>
+				    <div className="top-block"><span className="name">{name}</span><span className="identification main-grey-color">{nickname}</span></div>
+				    <div className="center-block"><span className="info">{text}</span></div>
+				    <div className="bottom-block">
+				      <div className="social-activity">
+				        <div className="social-parameters"><span className="social-param-name main-grey-color">Tweets:</span><span className="social-counter">{tweets}</span></div>
+				        <div className="social-parameters"><span className="social-param-name main-grey-color">Following:</span><span className="social-counter">{following}</span></div>
+				        <div className="social-parameters"><span className="social-param-name main-grey-color">Followers:</span><span className="social-counter">{followers}</span></div>
+				      </div>
+				    </div>
+				  </div>
+				</div>
 			</div>
 	    );
   	}
 }
 
 
-export default Item; 
+export default Item
--- client/reacttypeahead/components/ItemsList.tsx
+++ client/reacttypeahead/components/ItemsList.tsx
@@ -20,23 +20,32 @@ class ItemsList extends React.Component<ItemsListProps, ItemsListState> {
 	    }; 
 	}
 
+	handleItemClick(e, value) {
+		console.log("click");
+	}
+
 	render() {
 		const { items } = this.props;
 
-		let itemsTplList = items.map(function(item, index) {
-	        return (
-	          <div className="item-wrapper" key={index}>
-	            <Item item={item} />
-	          </div>
-	        )
-	      })
+		// let itemsTplList = items.map(function(item, index) {
+	 //        return (
+	 //          <div className="item-wrapper"
+	 //          	key={index}
+  //       	  >
+	 //            <Item
+	 //            	item={item} 
+	 //            	key={index}
+	 //            />
+	 //          </div>
+	 //        )
+	 //      })
 
 	    return(
 	      <div className="items-list-block">
 		    <div className="items-list-wrapper">
 		      <div className="items-list-inner">
 		        <div className="items-list">
-		          {itemsTplList}
+		          {items.map((item, index) => <Item item={item} key={index} />)}
 		        </div>
 		      </div>
 		    </div>
